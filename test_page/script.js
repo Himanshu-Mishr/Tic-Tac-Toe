b4f6341b42f087894f0319126ed387f0208853f8
Himanshu Mishra <himanshu.m786@gmail.com>
2015-12-24 18:19:36 +0530

    updating for checking changes

--- test_page/script.js
+++ test_page/script.js
@@ -5,6 +5,7 @@ function validateName() {
 	document.getElementById("name").attributes[0] = "form-control";
 
 	var name = document.getElementById("name").value;
+	if(name.length == 0) return;
 
 	if (/^[a-zA-Z ]+$/g.test(name) && name.length > 1) {
    		document.getElementById("name_class").attributes[0].value = "form-group row has-success";
@@ -14,6 +15,8 @@ function validateName() {
    		document.getElementById("name_class").attributes[0].value = "form-group row has-danger";
    		document.getElementById("name").attributes[0].value = "form-control form-control-danger";
 	}
+
+
 }
 
 
@@ -22,6 +25,7 @@ function validateEmail() {
 	document.getElementById("email").attributes[0] = "form-control";
 
 	var email = document.getElementById("email").value;
+	if(email.length == 0) return;
 
 	if (/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(email) && email.length > 1) {
    		document.getElementById("email_class").attributes[0].value = "form-group row has-success";
@@ -39,8 +43,9 @@ function validatePhone() {
 	document.getElementById("phone").attributes[0] = "form-control bfh-phone";
 
 	var phone = document.getElementById("phone").value;
+	if(phone.length == 0) return;
 
-	if (/\+\d\d \d\d\d\d\-\d\d\d\-\d\d\d/.test(phone) && phone.length > 1) {
+	if (/\d\d\d\d\d\d\d\d\d\d/.test(phone) && phone.length > 1) {
    		document.getElementById("phone_class").attributes[0].value = "form-group row has-success";
    		document.getElementById("phone").attributes[0].value = "form-control bfh-phone form-control-success";
 	}
@@ -48,6 +53,7 @@ function validatePhone() {
    		document.getElementById("phone_class").attributes[0].value = "form-group row has-danger";
    		document.getElementById("phone").attributes[0].value = "form-control bfh-phone form-control-danger";
 	}
+	document.getElementById("countryCode").attributes[0].value = "form-control form-control-primary";
 }
 
 
@@ -56,6 +62,7 @@ function validateFile() {
 	document.getElementById("resume").attributes[0] = "form-control";
 
 	var resume_value = document.getElementById("resume").value;
+	if(resume.length == 0) return;
 	resume_value = resume_value.split("\\")
 	var file_name = resume_value[resume_value.length-1].toLowerCase().split(".");
 	var file_ext = file_name[file_name.length-1];
@@ -76,7 +83,7 @@ function validateCoverLetter() {
 	document.getElementById("cover_letter").attributes[0] = "form-control";
 
 	var cover_letter_value = document.getElementById("cover_letter").value;
-
+	if(cover_letter_value.length == 0) return;
 
 	if (cover_letter_value.length > 1) {
    		document.getElementById("cover_letter_class").attributes[0].value = "form-group row has-success";
@@ -92,6 +99,57 @@ function restoreStyle(innerInput, outerFieldSet) {
 		document.getElementById(innerInput).attributes[0].value = "form-control form-control-primary";
 		document.getElementById(outerFieldSet).attributes[0].value = "form-group row";
 }
+
+// function to init form
+function init() {
+	// add year value in select fields.
+	var select_element = document.getElementById("countryCode");
+
+	var temp;
+
+	for(var country in country_phone_data)  {
+		temp = document.createElement("option");
+		temp.value = country_phone_data[country];
+		temp.innerHTML = country + " ( +" + country_phone_data[country] + " )";
+		select_element.appendChild(temp);
+	}
+}
+
+function validateURL(elementName, elementNameClass) {
+
+	document.getElementById(elementNameClass).attributes[0] = "form-group row";
+	document.getElementById(elementName).attributes[0] = "form-control";
+
+	var url_value = document.getElementById(elementName).value;
+
+
+	if(url_value.length == 0) return;
+
+	if(elementName == "URLlinkedin") {
+		var string =  url_value, substring = "linkedin";
+		if(string.indexOf(substring) > -1) {
+	   		document.getElementById(elementNameClass).attributes[0].value = "form-group row has-success";
+	   		document.getElementById(elementName).attributes[0].value = "form-control form-control-success";
+		}
+		else {
+	   		document.getElementById(elementNameClass).attributes[0].value = "form-group row has-danger";
+	   		document.getElementById(elementName).attributes[0].value = "form-control form-control-danger";
+		}
+	}
+
+	if(elementName == "URLgithub") {
+		var string =  url_value, substring = "github";
+		if (string.indexOf(substring) > -1) {
+	   		document.getElementById(elementNameClass).attributes[0].value = "form-group row has-success";
+	   		document.getElementById(elementName).attributes[0].value = "form-control form-control-success";
+		}
+		else {
+	   		document.getElementById(elementNameClass).attributes[0].value = "form-group row has-danger";
+	   		document.getElementById(elementName).attributes[0].value = "form-control form-control-danger";
+		}
+	}
+}
+
 
 
 
