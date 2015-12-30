1bee37be573e91f4a9c8bf192880c36b651f8eef
Himanshu Mishra <himanshu.m786@gmail.com>
2015-12-30 17:31:10 +0530

    updating for checking changes

--- test_page/mailDetails.php
+++ test_page/mailDetails.php
@@ -11,3 +11,19 @@ $URLlinkedin = $_POST['URLlinkedin'];
 $URLgithub = $_POST['URLgithub'];
 
 echo $name."--".$email."--".$countryCode."--".$phone."--".$resume."--".$coverLetter."--".$URLlinkedin."--".$URLgithub;
+
+
+$to =  $mail;
+$subject  = "Aplication for Software Developer Successfully accepted";
+$message = "
+Hello ".$name.",
+
+Thank you for your interest in Cozee Homes. We are working on some very exciting products with interesting technical challenges. We are looking for  motivated developers who are strong in programming, can pick up new technologies and can write elegant code.
+
+To determine whether you fit our requirements, we are sending you this small set of simple programming and analytical puzzles.
+
+Careers @ Cozee Homes
+
+"
+
+mail ( $to , $subject , $message);
